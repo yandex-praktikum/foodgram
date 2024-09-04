db0bd20e0262edc44477b4d4813fc23d6ee866d5
DeviusPav <pavel.ntg@gmail.com>
2024-09-04 09:46:12 +0500

    add_recipe_v1.0

--- backend/data/models.py
+++ backend/data/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
+
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+LENG_MAX = 256
+
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор'
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    name = models.CharField(
+        'Назвение',
+        max_length=LENG_MAX,
+        db_index=True,
+    )
+    description = models.TextField(
+        'Описание',
+        db_index=True,
+        max_length=LENG_MAX,
+        blank=True,
+    )
