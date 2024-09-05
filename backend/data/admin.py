83d8de438a13abd956456cef55fd544a594b39fa
DeviusPav <pavel.ntg@gmail.com>
2024-09-05 14:09:20 +0500

    recipe_vers2.1

--- backend/data/admin.py
+++ backend/data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, Ingredients, Tag
+
+admin.site.register(Recipe)
+admin.site.register(Ingredients)
+admin.site.register(Tag)
