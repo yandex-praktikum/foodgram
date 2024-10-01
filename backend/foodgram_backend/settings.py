9ee32e8f5b514cde4e186d90885f28dd5fba6520
DmitryMareninov <d7.m2.m2@yandex.ru>
2024-10-02 01:00:28 +0500

    Added and modified the files.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 
 # Статические файлы (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Тип поля первичного ключа по умолчанию
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
