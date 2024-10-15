048cf667154b6f367dacbfbd99933f9c293e1ae8
Saveli Rumiantsev <radogask9rsd@gmail.com>
2024-10-15 15:38:34 +0500

    add env, change db on postgres, add backend dockerfile and compose

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-28re$mtdnhu9cw-9leoic3615400x)5xmaeulvx74u@hsx8vsy'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_POST'),
     }
 }
 
