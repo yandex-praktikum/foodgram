6030dccce3ae782adab4f34e9ae32f46bc46b19a
Animeboygosha <animeboygosha@mail.ru>
2024-10-20 10:10:38 +0300

    add postgres

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "backend"]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
 
 # Application definition
